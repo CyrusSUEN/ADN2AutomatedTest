5b48d17c41a866b3381285c2706c4eaef8addb22
Cyrus S <yuyhero@gmail.com>
2016-03-29 12:42:26 +0800

    Update Chrome test to load extension file

--- src/test/PageVisitChromeTest.java
+++ src/test/PageVisitChromeTest.java
@@ -14,10 +14,16 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class PageVisitChromeTest {
+	
+	// Assuming the path of AdNauseam2 repo is next to ADN2AutomatedTest's
+	// Change the following path if needed
+	private static String extensionFilePath = 
+			System.getProperty("user.dir") + "/../AdNauseam2/dist/build/adnauseam.chromium.crx";	
 
 	private static ChromeDriverService service;
 	private WebDriver driver;
@@ -38,7 +44,13 @@ public class PageVisitChromeTest {
 
 	@Before
 	public void createDriver() {
-		driver = new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
+		ChromeOptions options = new ChromeOptions();
+		options.addExtensions(new java.io.File(extensionFilePath));
+		
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+		
+		driver = new RemoteWebDriver(service.getUrl(), capabilities);
 	}
 
 	@After
